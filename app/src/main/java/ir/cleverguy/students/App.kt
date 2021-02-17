09f97a3d87d9d5232e6e5ff5a3f4a23ea04388a3
CleverGuy <telecleverguy@gmail.com>
2021-02-17 12:34:07 +0330

    add modules in App.kt

---
+++ app/src/main/java/ir/cleverguy/students/App.kt
@@ -0,0 +1,44 @@
+package ir.cleverguy.students
+
+import android.app.Application
+import ir.cleverguy.students.data.repo.UserRepository
+import ir.cleverguy.students.data.repo.UserRepositoryImpl
+import ir.cleverguy.students.data.repo.source.UserLocalDataSource
+import ir.cleverguy.students.data.repo.source.UserRemoteDataSource
+import ir.cleverguy.students.feature.MainViewModel
+import ir.cleverguy.students.feature.StudentAdapter
+import ir.cleverguy.students.services.AppDatabase
+import ir.cleverguy.students.services.createApiServiceInstance
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        val myModules = module {
+            single { createApiServiceInstance() }
+
+            single { AppDatabase.getInstance(applicationContext).getStudentDao() }
+
+            single<UserRepository> {
+                UserRepositoryImpl(
+                    UserRemoteDataSource(get(), get()),
+                    UserLocalDataSource(get())
+                )
+            }
+
+            factory { StudentAdapter() }
+
+            viewModel { MainViewModel(get()) }
+        }
+
+        startKoin {
+            androidContext(this@App)
+            modules(myModules)
+        }
+    }
+}
