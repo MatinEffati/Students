9976039924d79170bb649cddf9a6fcc2137fbaac
CleverGuy <telecleverguy@gmail.com>
2021-02-17 12:45:19 +0330

    bug fixes

--- app/src/main/java/ir/cleverguy/students/feature/MainActivity.kt
+++ app/src/main/java/ir/cleverguy/students/feature/MainActivity.kt
@@ -1,6 +1,5 @@
 package ir.cleverguy.students.feature
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
