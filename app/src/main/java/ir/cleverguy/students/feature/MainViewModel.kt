77a3cd813ddf4894bc8c68e95f0e0f52e8b63c0e
CleverGuy <telecleverguy@gmail.com>
2021-02-17 12:31:28 +0330

    complete MainViewModel.kt

---
+++ app/src/main/java/ir/cleverguy/students/feature/MainViewModel.kt
@@ -0,0 +1,27 @@
+package ir.cleverguy.students.feature
+
+import androidx.lifecycle.LiveData
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import ir.cleverguy.students.common.AppCompletableObserver
+import ir.cleverguy.students.common.AppViewModel
+import ir.cleverguy.students.data.Student
+import ir.cleverguy.students.data.repo.UserRepository
+import timber.log.Timber
+
+class MainViewModel(val userRepository: UserRepository) : AppViewModel() {
+    init {
+        progressBarLiveData.value = true
+        userRepository.refreshStudent()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doFinally { progressBarLiveData.value = false }
+            .subscribe(object : AppCompletableObserver(compositeDisposable){
+                override fun onComplete() {
+                    Timber.i("Success")
+                }
+            })
+    }
+
+    fun getStudent():LiveData<List<Student>> = userRepository.getStudent()
+}
